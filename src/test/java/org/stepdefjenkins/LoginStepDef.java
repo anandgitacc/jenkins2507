103179f6b6bc427b425f83b36dca9e01c0051b7d
anandgitacc <anand.akmail@gmail.com>
2022-07-25 11:29:21 +0530

    finalgit

--- src/test/java/org/stepdefjenkins/LoginStepDef.java
+++ src/test/java/org/stepdefjenkins/LoginStepDef.java
@@ -1,5 +1,6 @@
 package org.stepdefjenkins;
 
+import org.baseclass.BaseClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,30 +9,31 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginStepDef {
+public class LoginStepDef extends BaseClass {
 	
 	//WebDriver driver;
 	
 	@Given("User is on facebook login page")
 	public void user_is_on_facebook_login_page() {
-		System.setProperty("webdriver.chrome.driver", 
-				"C:\\Users\\Welcome\\eclipse-workspace\\Jenkins2507\\target\\target\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		browserLaunch("https://en-gb.facebook.com/");
+		System.out.println("Browser launch");
 	}
 
 	@When("User should enter username and password")
 	public void user_should_enter_username_and_password() {
-		System.out.println("test1");
+		findElementid("email").sendKeys("hello");
+		findElementid("pass").sendKeys("hello@1123");
+		System.out.println("username and password");
 	}
 
 	@When("User should should click the login button")
 	public void user_should_should_click_the_login_button() {
-		System.out.println("test1");
+		System.out.println("login button");
 	}
 
 	@Then("User should verify successfully entered")
 	public void user_should_verify_successfully_entered() {
-	   
+	   System.out.println("success message");
 	}
 
 	
